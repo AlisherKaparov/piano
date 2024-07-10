555e9a677e55a2c806b770fe3471743af776e35e
AlisherKaparov <alisherkaparov2804@gmail.com>
2024-07-10 20:36:51 +0300

    axempl file koshuldu

---
+++ lib/axempl.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Axempl extends StatelessWidget {
+  const Axempl({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
